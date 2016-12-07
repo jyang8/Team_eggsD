8a9c67de2b8986b3ac7848c4f1c4aa3b91296136
root <root@XYZsjHome-HP.localdomain>
2016-12-07 18:07:09 -0500

    Fix issues with requests

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, redirect, url_for, request
-import utils
+from utils import nutrition
 #import flask
 #import sqlite3
 #import hashlib
@@ -21,10 +21,9 @@ def new():
 
 @app.route("/list/", methods = ["GET"])
 def list():
-    for (key in request.args):
+    for key in request.args:
         print key
-    if ('ingredient' in request.args):
-        
+    if 'ingredient' in request.args:
         list = nutrition.searchIngredient(request.args['ingredient']);
         return render_template('list.html', list = list)
     return redirect(url_for("new"))
