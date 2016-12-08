06bf583b82ae0863ea885e0e362e095c3c145b1e
Issac-Kim <ikim1@stuy.edu>
2016-12-07 22:07:56 -0500

    finish getRecipe

--- utils/recipes.py
+++ utils/recipes.py
@@ -11,7 +11,16 @@ def getRecipe(ingredients, dish):
         ingredients[index] = ingredients[index].replace(" ", "+")
         ingString += "&allowedIngredient[]=" + ingredients[index]
     link = "http://api.yummly.com/v1/api/recipes?_app_id=" + yid + "&_app_key=" + key + "&q=" + dish + ingString
-    print(ingString)
+    u = urllib2.urlopen(link)
+    D = json.loads(u.read())
+    if 'matches' in D:
+        recipes = {}
+        for recipe in D['matches']:
+            infoList = [recipe['sourceDisplayName'], recipe['ingredients'], recipe['totalTimeInSeconds'], recipe['smallImageUrls']]
+            recipes[recipe['recipeName']] = infoList
+        return recipes
+    else:
+        return "no matches"
 
 getRecipe(["hi  ", "two", "idjoa"], "this andosin asddhoia")
 
